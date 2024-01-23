3aaa0b1d76d89b6459eb6b457db43b27781e9d14
JGilbert <j.gilbert@mail.utoronto.ca>
2024-01-23 11:29:02 -0500

    First Draft.
    First draft and some graph building

--- scripts/02-data_cleaning.R
+++ scripts/02-data_cleaning.R
@@ -9,36 +9,51 @@
 
 #### Workspace setup ####
 library(tidyverse)
+library(dplyr)
 
 #### Clean data ####
-raw_data <- read_csv("inputs/data/plane_data.csv")
+raw_data <- read_csv("inputs/data/fire_incidents_data.csv")
 
 cleaned_data <-
   raw_data |>
   janitor::clean_names() |>
-  select(wing_width_mm, wing_length_mm, flying_time_sec_first_timer) |>
-  filter(wing_width_mm != "caw") |>
   mutate(
-    flying_time_sec_first_timer = if_else(flying_time_sec_first_timer == "1,35",
-                                   "1.35",
-                                   flying_time_sec_first_timer)
+    estimated_dollar_loss = ifelse(is.na(estimated_dollar_loss), 0, estimated_dollar_loss),
+    ignition_source = ifelse(is.na(ignition_source), "other", ignition_source),
+    is_highrise = as.numeric(grepl("highrise", initial_cad_event_type, ignore.case = TRUE)),
+    is_downtown = as.numeric(grepl("downtown", initial_cad_event_type, ignore.case = TRUE)),
+    location = case_when(
+      grepl("vehicle", initial_cad_event_type, ignore.case = TRUE) ~ "vehicle",
+      grepl("residential", initial_cad_event_type, ignore.case = TRUE) ~ "residential",
+      grepl("industrial", initial_cad_event_type, ignore.case = TRUE) ~ "commercial/industrial",
+      grepl("institution", initial_cad_event_type, ignore.case = TRUE) ~ "institution",
+      grepl("grass", initial_cad_event_type, ignore.case = TRUE) ~ "grass/rubbish",
+      grepl("subway", initial_cad_event_type, ignore.case = TRUE) ~ "subway",
+      TRUE ~ "other"),
+    ignition_source = case_match(ignition_source,
+      "11 - Stove, Range-top burner" ~ "Stove",
+      "71 - Smoker's Articles (eg. cigarettes, cigars, pipes already ignited" ~ "Smoker' Article",
+      "55 - Candle" ~ "Candle",
+      "77 - Matches or Lighters (unable to distinguish)" ~ "Matches",
+      "19 - Other Cooking Items (eg Toaster, Kettle, elec frying pan)" ~ "Cooking item",
+      "43 - Clothes Dryer" ~ "Dryer",
+      "79 - Other Open Flame Tools/Smokers' Articles" ~ "Open Flame"
+    )
   ) |>
-  mutate(wing_width_mm = if_else(wing_width_mm == "490",
-                                 "49",
-                                 wing_width_mm)) |>
-  mutate(wing_width_mm = if_else(wing_width_mm == "6",
-                                 "60",
-                                 wing_width_mm)) |>
-  mutate(
-    wing_width_mm = as.numeric(wing_width_mm),
-    wing_length_mm = as.numeric(wing_length_mm),
-    flying_time_sec_first_timer = as.numeric(flying_time_sec_first_timer)
-  ) |>
-  rename(flying_time = flying_time_sec_first_timer,
-         width = wing_width_mm,
-         length = wing_length_mm
-         ) |> 
-  tidyr::drop_na()
+  filter(is.numeric(incident_station_area)) |>
+  filter(!is.na(final_incident_type))
 
-#### Save data ####
-write_csv(cleaned_data, "outputs/data/analysis_data.csv")
+#### Save cleaned data ####
+write_csv(cleaned_data, "outputs/data/cleaned_fire_incident_data.csv")
+
+#### Data subset ####
+# Leaves behind all unused columns in the analysis 
+clean_subset  <-
+  select(cleaned_data, c("x_id", "area_of_origin", "incident_station_area", "civilian_casualties",
+                         "estimated_dollar_loss", "extent_of_fire", "ignition_source", "final_incident_type",
+                         "initial_cad_event_type", "method_of_fire_control", "tfs_alarm_time", "tfs_arrival_time"))
+
+fire_location_cost <-
+  select(cleaned_data, c("location", "is_highrise", "is_downtown", "estimated_dollar_loss", "ignition_source"))
+
+write_csv(fire_location_cost, "outputs/data/fire_event_data.csv")
--- scripts/04-model.R
+++ scripts/04-model.R
@@ -10,28 +10,93 @@
 
 #### Workspace setup ####
 library(tidyverse)
-library(rstanarm)
+library(ggplot2)
+library(dplyr)
 
 #### Read data ####
-analysis_data <- read_csv("outputs/data/analysis_data.csv")
+fire_event_data <- read_csv("outputs/data/fire_event_data.csv")
 
 ### Model data ####
-first_model <-
-  stan_glm(
-    formula = flying_time ~ length + width,
-    data = analysis_data,
-    family = gaussian(),
-    prior = normal(location = 0, scale = 2.5, autoscale = TRUE),
-    prior_intercept = normal(location = 0, scale = 2.5, autoscale = TRUE),
-    prior_aux = exponential(rate = 1, autoscale = TRUE),
-    seed = 853
+
+non_downtown_data <- fire_event_data %>%
+  filter(is_downtown == 0)
+downtown_data <- fire_event_data %>%
+  filter(is_downtown == 1)
+
+fire_event_costs <-
+  select(fire_event_data, c("location", "estimated_dollar_loss")) %>%
+  group_by(location) %>%
+  summarise(avg_dollar_loss = mean(estimated_dollar_loss))
+
+downtown_cost_data <-
+  select(fire_event_data, c("location", "estimated_dollar_loss")) %>%
+  group_by(location) %>%
+  summarise(avg_dollar_loss = mean(estimated_dollar_loss))
+
+dt_residential <-
+  downtown_data %>%
+  filter(location == "residential") |>
+  mutate(ignition_source = ave(ignition_source, ignition_source, FUN = function(i) replace(i, length(i) < 3, "other")))
+
+### GRAPHS ###
+
+fire_event_count <-
+  ggplot(fire_event_data, aes(location, fill = location)) +
+  geom_bar() +
+  labs(title = "Fire event type") +
+  theme_minimal() +
+  theme(
+    axis.text.x = element_text(angle = 45, hjust = 1),  # Rotate x-axis labels
+    legend.position = "right"
   )
 
+print(fire_event_count)
 
-#### Save model ####
-saveRDS(
-  first_model,
-  file = "outputs/models/first_model.rds"
-)
+fire_event_cost_graph <-
+  ggplot(fire_event_costs, aes(x = location, y = avg_dollar_loss, fill = location)) +
+  geom_col() +
+  theme_minimal() +
+  theme(
+    axis.text.x = element_text(angle = 45, hjust = 1),  # Rotate x-axis labels
+    legend.position = "right"
+  )
+
+print(fire_event_cost_graph)
+
+non_downtown_count_graph <-
+  ggplot(non_downtown_data, aes(location, fill = location)) +
+  geom_bar() +
+  labs(title = "Fire Location frequency - Downtown",
+       x = "Location") +
+  theme_minimal() +
+  theme(
+    axis.text.x = element_text(angle = 45, hjust = 1),  # Rotate x-axis labels
+    legend.position = "right"
+  )
+
+downtown_count_graph <-
+  ggplot(downtown_data, aes(location, fill = location)) +
+  geom_bar() +
+  labs(title = "Fire Location frequency - Downtown",
+       x = "Location") +
+  theme_minimal() +
+  theme(
+    axis.text.x = element_text(angle = 45, hjust = 1),  # Rotate x-axis labels
+    legend.position = "right"  # Remove legend
+  )
+
+print(downtown_count_data)
+
+downtown_cost_graph <-
+  ggplot(downtown_cost_data, aes(x = location, y = avg_dollar_loss, fill = location)) +
+  geom_col() +
+  theme_minimal()
 
 
+print(downtown_cost_graph)
+
+dt_residential_cause <-
+  ggplot(dt_residential, aes(x = ignition_source)) +
+  geom_bar()
+
+print(dt_residential_cause)
